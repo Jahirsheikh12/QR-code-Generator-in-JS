21c0b5fd367a683f2baa2fd32628e0d38715c248
jahir sheikh <jahirsheikh211@gmail.com>
2024-02-27 01:31:43 +0530

    QR Code app created successfully

---
+++ script.js
@@ -0,0 +1,33 @@
+let URL = document.getElementById('url-input');
+
+let generateBtn = document.getElementById('generate');
+
+let qrContainer = document.querySelector('.qr-container');
+
+let resetBtn = document.getElementById('reset');
+
+
+generateBtn.addEventListener('click' , () => {
+    let encodeUrl = encodeURIComponent(URL.value);
+    let apiUrl= `https://api.qrserver.com/v1/create-qr-code/?data=${encodeUrl}&size=100x100`;
+    console.log(encodeUrl)
+    let imgContainer = document.createElement('img');
+    imgContainer.src = apiUrl;
+    qrContainer.appendChild(imgContainer);
+    if(URL.value === ''){
+        alert("please enter a text or valid URL");
+    } else {
+        qrContainer.style.display = "inline";
+    }
+    generateBtn.style.display = "none";
+    
+    resetBtn.style.display = "block";
+
+});
+
+// resetBtn.addEventListener('click', () => {
+//     URL.value = '';
+//     qrContainer.remove();
+//     generateBtn.style.display = 'block';
+//     resetBtn.style.display = "none";
+// });
